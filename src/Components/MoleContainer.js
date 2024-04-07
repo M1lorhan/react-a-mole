10c1da7dcaccc25299bdd792f71fcf69b3a1ced0
Marshall Lorhan <mlorhan@gmail.com>
2024-04-06 18:42:02 -0700

    added game functionality

---
+++ src/Components/MoleContainer.js
@@ -0,0 +1,21 @@
+import React, { useState } from 'react';
+import Mole from './Mole';
+import EmptySlot from './EmptySlot';
+
+function MoleContainer (props) {
+    let [theMole, setTheMole] = useState(false)
+
+    const handleClick = (e) => {
+        props.setScore(props.score +100)
+        setTheMole(false)
+    }
+    let displayMole = theMole ? <Mole toggle={setTheMole} handleClick={handleClick} /> :<EmptySlot toggle={setTheMole} />
+   
+    return (
+        <div style={{'display': 'inline-block', 'width': '30vh'}}>
+            {displayMole}
+        </div>
+    )
+}
+
+export default MoleContainer
